145c9d6fa4a0828ed556093ea9c821ae61c00de1
stevecyril45 <stevechibuikecyril@gmail.com>
2025-09-24 08:32:23 +0100

    v

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
-export class ServicesComponent implements OnInit {
+export class ServicesComponent implements AfterViewInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    ($('.owl-carousel') as any).owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: true,
+      dots: false,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: { items: 1 },
+        768: { items: 1 },
+        1200: { items: 1 }
+      }
+    });
   }
-
 }
